83728f105c689964039b995fd61099c59dc03a9c
Ahmed Rizk <rezk18649@gmail.com>
2025-08-10 14:51:33 +0300

    initialize app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget{
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
+
