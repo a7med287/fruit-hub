815790ffd85d65a48ec488827d5538efdcb5ed0e
Ahmed Rizk <rezk18649@gmail.com>
2025-08-10 15:21:22 +0300

    create on generate routs

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class SplashView extends StatelessWidget{
   const SplashView({super.key});
 
+  static const String routeName = "SplashView" ;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/helper/on_generate_routs.dart';
 import 'features/splash/presentation/views/splash_view.dart';
 
 void main() {
@@ -11,7 +12,8 @@ class FruitHub extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const SplashView(),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
     );
   }
 }
