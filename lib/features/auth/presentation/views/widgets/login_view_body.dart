8338ec3210d2391e83b88a2e22ed1234dc3c5393
Ahmed Rizk <rezk18649@gmail.com>
2025-08-12 23:05:37 +0300

    add text forgot password & custom button

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/constants.dart';
+import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_text_styles.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
 
 import '../../../../../core/widgets/custom_text_field.dart';
 import '../../../../../generated/l10n.dart';
@@ -32,7 +34,6 @@ class LoginViewBody extends StatelessWidget {
                 ),
               ),
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
@@ -45,6 +46,18 @@ class LoginViewBody extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomButton(text: "تسجيل دخول"),
+            SizedBox(height: 32,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("لا تمتلك حساب؟  ",style: TextStyles.semiBold16.copyWith(color: Color(0xff616A6B)),),
+                Text("قم بإنشاء حساب",style: TextStyles.semiBold16.copyWith(color: AppColors.primaryColor),)
+              ],
+            )
           ],
         ),
       ),
