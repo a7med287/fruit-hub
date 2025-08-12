11d1112761457454629afecf720ca80fdaaacbc0
Ahmed Rizk <rezk18649@gmail.com>
2025-08-12 22:38:49 +0300

    add password text form field

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -2,22 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:fruit_hub/constants.dart';
 
 import '../../../../../core/widgets/custom_text_field.dart';
-class LoginViewBody extends StatelessWidget{
+
+class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-
       padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
       child: SingleChildScrollView(
         child: Column(
           children: [
-              CustomTextFormField(hintText: 'البريد الإلكتروني',)
+            SizedBox(height: 24),
+            CustomTextFormField(
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.remove_red_eye_rounded,
+                  color: Color(0xffC9CECF),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
